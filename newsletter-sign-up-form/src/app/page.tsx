99342bde48495d4bdaafd50208d7778f5d72ce76
tuansdf <103062682+tuansdf@users.noreply.github.com>
2023-06-26 18:35:36 +0700

    add mobile

--- newsletter-sign-up-form/src/app/page.tsx
+++ newsletter-sign-up-form/src/app/page.tsx
@@ -1,9 +1,35 @@
 import { Attribution } from "~/components/attribution";
+import classes from "./page.module.scss";
 
 export default function Home() {
   return (
     <>
-      <main>Next Template</main>;
+      <main className={classes["main"]}>
+        <img
+          src="/images/illustration-sign-up-mobile.svg"
+          alt=""
+          className={classes["image"]}
+        />
+
+        <div className={classes["body"]}>
+          <h1 className={classes["title"]}>Stay updated!</h1>
+          <p className={classes["subtitle"]}>
+            Join 60,000+ product managers receiving monthly updates on:
+          </p>
+          <ul className={classes["list"]}>
+            <li>Product discovery and building what matters</li>
+            <li>Measuring to ensure updates are a success</li>
+            <li>And much more!</li>
+          </ul>
+
+          <form className={classes["form"]}>
+            <label>Email address</label>
+            <input type="email" placeholder="email@company.com" />
+            <button>Subscribe to monthly newsletter</button>
+          </form>
+        </div>
+      </main>
+
       <Attribution />
     </>
   );
--- newsletter-sign-up-form/src/components/attribution.tsx
+++ newsletter-sign-up-form/src/components/attribution.tsx
@@ -3,7 +3,7 @@ import classes from "./attribution.module.scss";
 export const Attribution = () => {
   return (
     <div className={classes["attribution"]}>
-      Challenge by
+      Challenge by{" "}
       <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
         Frontend Mentor
       </a>
